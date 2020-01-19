28832199da355adbabd7ae1190df42a0398c68ee
Anna Levenberg <alevenb1@binghamton.edu>
2020-01-19 12:50:51 -0500

    Add mask word cloud img

--- word_cloud.py
+++ word_cloud.py
@@ -1,10 +1,14 @@
 
-from wordcloud import WordCloud, STOPWORDS 
-import string 
 import matplotlib.pyplot as plt 
+import numpy as np 
+from PIL import Image
+import string 
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 def generate_cloud(words):
-    wordcloud = WordCloud(width=480, height=480, margin=0).generate(words)
+    wc = WordCloud(width=480, height=480, margin=0)
+    wc.generate(words)
+    wc.to_file('word_cloud.png')
 
     # Plot the WordCloud image                        
     plt.figure(figsize = (8, 8), facecolor = None) 
@@ -12,10 +16,18 @@ def generate_cloud(words):
     plt.axis("off") 
     plt.tight_layout(pad = 0) 
 
-    plt.savefig('word_cloud.png')
-
     # To view the figure
     plt.show() 
+
+def generate_mask_word_cloud(words, mask):
+    char_mask = np.array(Image.open(mask, 'r').convert('RGB')) 
+   
+    wc = WordCloud(background_color="white", max_words=200, width=400, height=400, mask=char_mask)
+    wc.generate(words)
+    wc.to_file('mask_word_cloud.png')
+
+    # plt.imshow(wc.recolor(color_func=image_colors))
+    # plt.show() 
 
 def main():
     # Replace with correct file name 
@@ -25,7 +37,10 @@ def main():
     
     words = file.readline()
 
-    generate_cloud(words)
+    # generate_cloud(words)
+
+    mask_file = "phineas-mask.jpg"
+    generate_mask_word_cloud(words, mask_file)
 
 if __name__ == "__main__": 
     main()    
